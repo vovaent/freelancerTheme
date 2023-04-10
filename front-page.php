3ebe964390ad8fb6e313f4e0f191e850a899bda8
vovaent <vovaent@gmail.com>
2023-04-10 19:05:06 +0300

    Front Page Template Created

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Front Page Template
+ *
+ * Description: A page template that provides a key component of WordPress as a CMS
+ * by meeting the need for a carefully crafted introductory page. The front page template
+ * in Twenty Twelve consists of a page content area for adding text, images, video --
+ * anything you'd like -- followed by front-page-only widgets in one or two columns.
+ *
+ * @package freelancerTheme
+ */
+
+ get_header('front'); ?>
+
+
+ <?php get_footer(); ?>
