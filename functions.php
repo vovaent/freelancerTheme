5235166e15d26c2b5237643a8383b4d0a4edaba3
vovaent <vovaent@gmail.com>
2023-04-12 23:23:24 +0300

    Bootstrap@5.2.3 js script added

--- functions.php
+++ functions.php
@@ -139,11 +139,12 @@ add_action( 'widgets_init', 'freelancertheme_widgets_init' );
  */
 function freelancertheme_scripts() {
 	wp_enqueue_style( 'freelancertheme-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_enqueue_style( 'freelancertheme-landing-page-styles', get_template_directory_uri() . '/css/styles.css', array('freelancertheme-style'), _S_VERSION );
+	wp_enqueue_style( 'freelancertheme-main-styles', get_template_directory_uri() . '/css/styles.css', array( 'freelancertheme-style' ), _S_VERSION );
 	wp_style_add_data( 'freelancertheme-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'freelancertheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-	wp_enqueue_script( 'freelancertheme-landing-page-scripts', get_template_directory_uri() . '/js/scripts.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'freelancertheme-bootstrap@5.2.3', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'freelancertheme-main-scripts', get_template_directory_uri() . '/js/scripts.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
