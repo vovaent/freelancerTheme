4675876fb3bffc4ee866da610d525b3b393a87f2
vovaent <vovaent@gmail.com>
2023-04-13 21:09:24 +0300

    Minor fixes

--- inc/optimization.php
+++ inc/optimization.php
@@ -7,10 +7,11 @@
 
 // Disable self-pingbacks
 function freelancerTheme_stop_self_ping( &$links ) {
-    $home = get_option( 'home' );
-    foreach ( $links as $l => $link )
-        if ( 0 === strpos( $link, $home ) )
-            unset($links[$l]);
+	$home = get_option( 'home' );
+	foreach ( $links as $l => $link ) {
+		if ( 0 === strpos( $link, $home ) ) {
+			unset( $links[ $l ] );
+		}
+	}
 }
- 
-add_action( 'pre_ping', 'stop_self_ping' );
+add_action( 'pre_ping', 'freelancerTheme_stop_self_ping' );
